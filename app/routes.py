7f26559710309d398b976deeb44aeab596c5f79a
Julie Keosourinha <jkeosourinha@gmail.com>
2026-06-28 20:14:05 +0000

    ..

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return "🚀 Summer Startup API is live"
+    return render_template("index.html")
